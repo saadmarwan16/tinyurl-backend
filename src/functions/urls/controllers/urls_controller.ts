374217cd1dfbc6323f9df67f628b28fac867125e
saadmarwan16 <saadmarwan16@gmail.com>
2023-05-24 16:02:40 +0300

    Added github actions workflows

--- src/functions/urls/controllers/urls_controller.ts
+++ src/functions/urls/controllers/urls_controller.ts
@@ -1,18 +1,23 @@
 import { Controller, Get, Path, Route } from 'tsoa';
+import { config } from 'dotenv';
+
+config();
 
 @Route('/urls')
 export class UrlsController extends Controller {
 	@Get()
 	public getAllUrls() {
 		return {
-			message: 'Hello, world',
+			message: `Hello, world from STAGE ${process.env.STAGE as string}`,
 		};
 	}
 
 	@Get('/{id}')
 	public getSingleUrl(@Path('id') id: string) {
 		return {
-			message: `Hello, world with id of ${id}`,
+			message: `Hello, world from STAGE ${
+				process.env.STAGE as string
+			} with id of ${id}`,
 		};
 	}
 }
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+// import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { app } from './app';
-import { createProductsTableIfDoesNotExist } from './utils/createProductsTableIfDoesNotExist';
+// import { createProductsTableIfDoesNotExist } from './utils/createProductsTableIfDoesNotExist';
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-setImmediate(async () => {
-	await createProductsTableIfDoesNotExist(
-		new DynamoDBClient({
-			endpoint: 'http://localhost:8000',
-			region: 'local',
-		})
-	);
-});
+// // eslint-disable-next-line @typescript-eslint/no-misused-promises
+// setImmediate(async () => {
+// 	await createProductsTableIfDoesNotExist(
+// 		new DynamoDBClient({
+// 			endpoint: 'http://localhost:8000',
+// 			region: 'local',
+// 		})
+// 	);
+// });
 
 const PORT = 3000;
 
